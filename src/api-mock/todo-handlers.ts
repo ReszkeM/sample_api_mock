c267da8dd2841ad1ae1957c0b4835dcbcb6543a1
ReszkeM <marek.reszke09@gmail.com>
2020-11-11 18:55:42 +0100

    extend api handlers to use localStorageDB

--- src/api-mock/todo-handlers.ts
+++ src/api-mock/todo-handlers.ts
@@ -1,34 +1,62 @@
 import { rest } from 'msw';
+import { getDatabase } from '../database-config/setup-database';
 
 export const todoHandlers = [
   rest.get('https://sample-api-mock.com/todos', (req, res, ctx) => {
+    const todos = getDatabase().queryAll('todos');
+
     return res(
       ctx.delay(1000),
-      ctx.status(202, 'Mocked status'),
-      ctx.json({
-        message: 'Mocked response JSON body'
-      }),
+      ctx.status(200),
+      ctx.json(todos),
+    )
+  }),
+  rest.get('https://sample-api-mock.com/todos/:todoId', (req, res, ctx) => {
+    const { todoId } = req.params;
+
+    const todos = getDatabase().queryAll("todos", { query: { ID: todoId } });
+
+    return res(
+      ctx.delay(1000),
+      ctx.status(200),
+      ctx.json(todos),
     )
   }),
   rest.post('https://sample-api-mock.com/todos', (req, res, ctx) => {
+    const db = getDatabase();
+    db.insert("todos", req.body);
+    db.commit();
+
     return res(
       ctx.delay(1000),
-      ctx.status(202, 'Mocked status')
+      ctx.status(200),
     )
   }),
-  rest.put('https://sample-api-mock.com/todos', (req, res, ctx) => {
+  rest.put('https://sample-api-mock.com/todos:todoId', (req, res, ctx) => {
+    const { todoId } = req.params;
+
+    const db = getDatabase();
+    db.update("todos", { ID: todoId }, () => req.body);
+    db.commit();
+
     return res(
       ctx.delay(1000),
-      ctx.status(403),
+      ctx.status(200),
       ctx.json({
         errorMessage: 'Not authorized',
       }),
     )
   }),
-  rest.delete('https://sample-api-mock.com/todos', (req, res, ctx) => {
+  rest.delete('https://sample-api-mock.com/todos/:todoId', (req, res, ctx) => {
+    const { todoId } = req.params;
+
+    const db = getDatabase();
+    db.deleteRows("todos", { ID: todoId });
+    db.commit();
+
     return res(
       ctx.delay(1000),
-      ctx.status(202, 'Mocked status')
+      ctx.status(200),
     )
   })
 ];
